8423ccba7cd6c52e393c746db8d418875a312c13
zhenxing <zhenxing@qh-speed.com>
2017-05-27 17:40:35 +0800

    happy

--- app/src/main/java/com/test/basecustomview/act/ThirdActivity.java
+++ app/src/main/java/com/test/basecustomview/act/ThirdActivity.java
@@ -1,15 +1,86 @@
 package com.test.basecustomview.act;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.test.basecustomview.R;
+import com.test.basecustomview.view.CustomDialog;
 
 public class ThirdActivity extends AppCompatActivity {
+
+    private Button btnShowDialogFragment;
+    private Button btnShowDialog1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_acvtivity);
+
+        initView();
+
+        initListener();
+
+
+    }
+
+    private void initListener() {
+        btnShowDialogFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogFragment();
+            }
+        });
+
+        btnShowDialog1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog1();
+            }
+        });
+    }
+
+    /*
+     * 这里导的是 v7 包 里面的 AlertDialog
+     * 这样显示也是默认居中的
+     */
+    private void showDialog1() {
+        new AlertDialog.Builder(ThirdActivity.this)
+                .setTitle("自定义布局")
+                .setView(R.layout.dialog_ar_land).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(ThirdActivity.this,"点击确定",Toast.LENGTH_SHORT).show();
+            }
+        })
+        .show();
+
+
+}
+
+    private void initView() {
+        btnShowDialogFragment = (Button) findViewById(R.id.btnShowDialogFragment);
+        btnShowDialog1 = (Button) findViewById(R.id.btnShowDialog1);
+    }
+
+    private void showDialogFragment() {
+        CustomDialog customDialog = new CustomDialog();
+
+        customDialog.setListener(new CustomDialog.MyListener() {
+            @Override
+            public void cancel() {
+                Toast.makeText(ThirdActivity.this,"cancel",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void confirm() {
+                Toast.makeText(ThirdActivity.this,"confirm",Toast.LENGTH_SHORT).show();
+            }
+        });
+        customDialog.show(getFragmentManager(), "customDialog");
     }
 }
---
+++ app/src/main/java/com/test/basecustomview/view/CustomDialog.java
@@ -0,0 +1,82 @@
+package com.test.basecustomview.view;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import com.test.basecustomview.R;
+
+
+/**
+ * DialogFragment 不管布局是什么样的，这个 Dialog 都是居中的
+ *
+ */
+public class CustomDialog extends DialogFragment {
+
+    private MyListener MyListener;
+    private Button btnCancel;
+    private Button btnConfirm;
+    private TextView tvContent;
+    private LinearLayout layoutBottom;
+
+    public void setListener(MyListener listener) {
+        this.MyListener = listener;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getDialog().setCanceledOnTouchOutside(true);
+        getDialog().getWindow().setBackgroundDrawableResource(R.color.transparent);
+
+        Window window = getDialog().getWindow();
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT; // LayoutParams --> ViewGroup 是可以的
+        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT; // LayoutParams --> FrameLayout 是可以的...甚至其他莫名其妙的 LinearLayout,TableLayout 都可以
+        layoutParams.gravity = Gravity.BOTTOM;
+        window.setAttributes(layoutParams);
+
+        View view = inflater.inflate(R.layout.dialog_ar_land, container, false);
+        btnCancel = (Button) view.findViewById(R.id.btnCancel);
+        btnConfirm = (Button) view.findViewById(R.id.btnConfirm);
+        tvContent = (TextView) view.findViewById(R.id.tv_content);
+        layoutBottom = (LinearLayout) view.findViewById(R.id.layout_bottom);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyListener.cancel();
+                dismiss();
+            }
+        });
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyListener.confirm();
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+
+    public interface MyListener {
+        void cancel();
+
+        void confirm();
+    }
+}
